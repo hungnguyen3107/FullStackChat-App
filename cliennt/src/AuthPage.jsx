8e4a76f27b03087b5bb65262fa8ffd1d4aa41c24
Nguyễn Thúc Hoàng Hùng <103067871+hungnguyen3107@users.noreply.github.com>
2023-12-30 21:29:54 +0700

    Update AuthPage.jsx

--- cliennt/src/AuthPage.jsx
+++ cliennt/src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
       e.preventDefault();
       const { value } = e.target[0];
       axios.post(
-        'https://abc-33sz.onrender.com/authenticate',
+        'http://localhost:3001/authenticate',
         {username: value}
         ).then(r =>props.onAuth({ ...r.data, secret: value }) )
          .catch(e => console.log('error',e))
